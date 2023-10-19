e778a077e5e601b807f5b4cb1472d6aa3a40cec2
Rishabh IO <fourofour@fourofour.local>
2023-10-19 21:32:45 +0530

    Added section on Sets

---
+++ section-12/sets-intro.py
@@ -0,0 +1,20 @@
+
+
+# 1. A unique collection 
+# 2. Unordered 
+# 3. Unindexed 
+# 4. Mutable 
+
+
+'''
+    Syntax: 
+        [], 
+        (), 
+        {} 
+    Reuse the brackets 
+'''
+
+set_identifier = {1, 2, 3, 4, 4, 4, 4, 5 }
+print(set_identifier)
+# set_name = set() 
+
